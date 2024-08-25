c3a04438fd8de7fd19b3db017639a31a5799fe55
Charles Lo <jerries.lo@gmail.com>
2024-08-25 19:32:37 +0800

    rename the main so file to ffplay

--- ffplay/app/src/main/java/com/charles/app/FFPlay.java
+++ ffplay/app/src/main/java/com/charles/app/FFPlay.java
@@ -10,4 +10,11 @@ public class FFPlay extends SDLActivity {
     protected String[] getArguments() {
         return new String[]{"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", "-x", String.valueOf(mSurface.getWidth()), "-y", String.valueOf(mSurface.getHeight())};
     }
+
+    protected String[] getLibraries() {
+        return new String[] {
+                "SDL2",
+                "ffplay"
+        };
+    }
 }
